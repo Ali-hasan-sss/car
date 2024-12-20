0e90a557aae3b42f72147698523db5c57300b655
Ali-hasan-sss <emonate8@gmail.com>
2024-12-20 09:07:58 +0300

    add new about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import "./style.css";
+import About_header from "./header";
+import About_section1 from "./section1";
+import About_section2 from "./section2";
+
+export default function About() {
+  return (
+    <div>
+      <About_header />
+      <About_section1 />
+      <About_section2 />
+    </div>
+  );
+}
---
+++ src/app/about/section1/index.tsx
@@ -0,0 +1,34 @@
+export default function About_section1() {
+  return (
+    <div className="py-[30px] md:py-[100px] px-[10px] md:px-[60px] bg-white ">
+      <div className="block md:flex gap-[80px] items-center justify-between  ">
+        <div className=" w-full py-4 md:w-1/2">
+          <h1 className="title1">
+            Tell the story of how your company came about
+          </h1>
+        </div>
+        <div className="w-full py-4 md:w-1/2">
+          <p className="des1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            varius faucibus massa sollicitudin amet augue. Nibh metus a semper
+            purus mauris duis. Lorem eu neque, tristique quis duis. Nibh
+            scelerisque ac adipiscing velit non nulla in amet pellentesque. Sit
+            turpis pretium eget maecenas. Vestibulum dolor mattis consectetur
+            eget commodo vitae. Amet pellentesque sit pulvinar lorem mi a,
+            euismod risus rhoncus. Elementum ullamcorper nec, habitasse
+            vulputate. Eget dictum quis est sed egestas tellus, a lectus. Quam
+            ullamcorper in fringilla arcu aliquet fames arcu.Lacinia eget
+            faucibus urna, nam risus nec elementum cras porta. Sed elementum,
+            sed dolor purus dolor dui. Ut dictum nulla pulvinar vulputate sit
+            sagittis in eleifend dignissim. Natoque mauris cras molestie velit.
+            Maecenas eget adipiscing quisque viverra lectus arcu, tincidunt
+            ultrices pellentesque.
+          </p>
+        </div>
+      </div>
+      <div className="placeholder w-full ">
+        <img src="/images/Placeholder.png" alt="Placeholder" />
+      </div>
+    </div>
+  );
+}
